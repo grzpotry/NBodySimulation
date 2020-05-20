ba190f84c9a679d06859776a50035572ff263d67
grzpotry <grzpotry@gmail.com>
2020-05-20 23:59:20 +0200

    Inited U4 project

---
+++ Source/SolarSystem/SolarSystem.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SolarSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SolarSystem, "SolarSystem" );
+ 
---
+++ Source/SolarSystem/SolarSystemGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SolarSystemGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASolarSystemGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASolarSystemGameMode();
+};
+
+
+
